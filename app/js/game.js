385b0ac071ce547c847ed6e7db5b2934e3c3a66d
Bruno <endo.bruno@gmail.com>
2016-11-30 21:01:39 -0200

    ae

--- app/js/game.js
+++ app/js/game.js
@@ -1,224 +1,100 @@
-// namespace our game
-var GAME = {
 
-    // set up some initial values
-    WIDTH: 320, 
-    HEIGHT:  480, 
-    // we'll set the rest of these
-    // in the init function
-    RATIO:  null,
-    currentWidth:  null,
-    currentHeight:  null,
-    canvas: null,
-    ctx:  null,
+var canvas;
+var ctx;
+var WIDTH = 400;
+var HEIGHT = 400;
+var rects = new Array();
 
-    // let's keep track of scale
-    // along with all initial declarations
-    // at the start of the program
-    scale:  1,
-    // the position of the canvas
-    // in relation to the screen
-    offset: {top: 0, left: 0},
-
-    init: function() {
-
-        // the proportion of width to height
-        GAME.RATIO = GAME.WIDTH / GAME.HEIGHT;
-
-        // these will change when the screen is resized
-        GAME.currentWidth = GAME.WIDTH;
-        GAME.currentHeight = GAME.HEIGHT;
-
-        // this is our canvas element
-        GAME.canvas = document.getElementsByTagName('canvas')[0];
-
-        // setting this is important
-        // otherwise the browser will
-        // default to 320 x 200
-        GAME.canvas.width = GAME.WIDTH;
-        GAME.canvas.height = GAME.HEIGHT;
-
-        // the canvas context enables us to 
-        // interact with the canvas api
-        GAME.ctx = GAME.canvas.getContext('2d');
-
-        // we need to sniff out Android and iOS
-        // so that we can hide the address bar in
-        // our resize function
-        GAME.ua = navigator.userAgent.toLowerCase();
-        GAME.android = GAME.ua.indexOf('android') > -1 ? true : false;
-        GAME.ios = ( GAME.ua.indexOf('iphone') > -1 || GAME.ua.indexOf('ipad') > -1  ) ? 
-            true : false;
-
-        // we're ready to resize
-        GAME.resize();
-
-        // include this at the end of GAME.init function
-        GAME.Draw.clear();
-        // GAME.Draw.rect(120,120,150,150, 'green');
-        // GAME.Draw.rect(20,20,150,150, 'green');
-        // GAME.Draw.rect(0,0,150,150, 'red');
-
-        console.log("estou aca");
-
-
-        makeGrid(8, 16, GAME.currentWidth/8, GAME.currentHeight/16);
-
-
-    },
-
-    resize: function() {
-
-        GAME.currentHeight = window.innerHeight;
-        // resize the width in proportion
-        // to the new height
-        GAME.currentWidth = GAME.currentHeight * GAME.RATIO;
-
-        // this will create some extra space on the
-        // page, allowing us to scroll past
-        // the address bar, thus hiding it.
-        if (GAME.android || GAME.ios) {
-            document.body.style.height = (window.innerHeight + 50) + 'px';
-        }
-
-        // set the new canvas style width and height
-        // note: our canvas is still 320 x 480, but
-        // we're essentially scaling it with CSS
-        GAME.canvas.style.width = GAME.currentWidth + 'px';
-        GAME.canvas.style.height = GAME.currentHeight + 'px';
-
-        // add this to the resize function.
-        GAME.scale = GAME.currentWidth / GAME.WIDTH;
-        console.log(GAME.canvas);
-        GAME.offset.top = GAME.canvas.offsetTop;
-        GAME.offset.left = GAME.canvas.offsetLeft;
-
-        // we use a timeout here because some mobile
-        // browsers don't fire if there is not
-        // a short delay
-        window.setTimeout(function() {
-                window.scrollTo(0,1);
-        }, 1);
-    }
-
-};
-
-// abstracts various canvas operations into
-// standalone functions
-GAME.Draw = {
-
-    clear: function() {
-        GAME.ctx.clearRect(0, 0, GAME.WIDTH, GAME.HEIGHT);
-    },
-
-    rect: function(x, y, w, h, col) {
-        GAME.ctx.fillStyle = col;
-        GAME.ctx.fillRect(x, y, w, h);
-    },
-
-    circle: function(x, y, r, col) {
-        GAME.ctx.fillStyle = col;
-        GAME.ctx.beginPath();
-        GAME.ctx.arc(x + 5, y + 5, r, 0,  Math.PI * 2, true);
-        GAME.ctx.closePath();
-        GAME.ctx.fill();
-    },
-
-    text: function(string, x, y, size, col) {
-        GAME.ctx.font = 'bold '+size+'px Monospace';
-        GAME.ctx.fillStyle = col;
-        GAME.ctx.fillText(string, x, y);
-    }
-
-};
-
-window.addEventListener('load', GAME.init, false);
-window.addEventListener('resize', GAME.resize, false);
-
-
-// + add this at the bottom of your code,
-// before the window.addEventListeners
-GAME.Input = {
-
-    x: 0,
-    y: 0,
-    tapped :false,
-
-    set: function(data) {
-
-        var offsetTop = GAME.canvas.offsetTop,
-            offsetLeft = GAME.canvas.offsetLeft;
-            // scale = GAME.currentWidth / GAME.WIDTH;
-
-        this.x = ( data.pageX - offsetLeft ) / GAME.scale;
-        this.y = ( data.pageY - offsetTop ) / GAME.scale;
-        this.tapped = true; 
-
-        GAME.Draw.circle(this.x, this.y, 10, 'red');
-    }
-
-};
-
-
-// listen for clicks
-window.addEventListener('click', function(e) {
-    e.preventDefault();
-    GAME.Input.set(e);
-}, false);
-
-// listen for touches
-window.addEventListener('touchstart', function(e) {
-    e.preventDefault();
-    // the event object has an array
-    // named touches; we just want
-    // the first touch
-    GAME.Input.set(e.touches[0]);
-}, false);
-window.addEventListener('touchmove', function(e) {
-    // we're not interested in this,
-    // but prevent default behaviour
-    // so the screen doesn't scroll
-    // or zoom
-    e.preventDefault();
-}, false);
-window.addEventListener('touchend', function(e) {
-    // as above
-    e.preventDefault();
-}, false);
-
-
-var makeGrid = function(rows, columns, horSize, verSize) {
-    console.log("bla", rows, columns, horSize, verSize);
-
-    for( var i = 0; i < rows; i++){
-        for( var j = 0; j < columns; j++)
-        {
-            // GAME.Draw.rect(i * horSize, j * verSize, 
-            //                 i * horSize + horSize, j * verSize + verSize, 
-            //                     'red');
-            if (i % 2 == 0) 
-                if (j % 2 == 0)    
-                    GAME.Draw.rect(i * horSize, j * verSize, 
-                             horSize, verSize, 
-                                'red');
-                else
-                    GAME.Draw.rect(i * horSize, j * verSize, 
-                             horSize, verSize, 
-                                'blue');
-            else 
-                if (j % 2 == 0)
-                    GAME.Draw.rect(i * horSize, j * verSize, 
-                            horSize, verSize, 
-                                'green');
-                else
-                    GAME.Draw.rect(i * horSize, j * verSize, 
-                            horSize, verSize, 
-                                'yellow'); 
-            GAME.Draw.rect(i*horSize, j*verSize, horSize, 1, 'black');
-            GAME.Draw.rect(i*horSize, j*verSize, 1, 2*verSize, 'black');
-            // GAME.Draw.rect(0,0,20,20,'green');
-            // GAME.ctx.fillRect(0, 0, 20, 20);
-            console.log("estou aqui " + i +" " + j)       
-        }
-    }
+function rect (x, y, w, h) {
+	ctx.beginPath();
+	ctx.rect (x, y, w, h);
+	ctx.closePath();
+	ctx.fill ();
+	ctx.stroke ();
 }
+
+function clear (i, j, w, h) {
+	ctx.clearRect (i, j, w, h);
+}
+
+function init() {
+	canvas = document.getElementById ("canvas");
+	ctx = canvas.getContext ("2d");
+	createGrid ();
+}
+
+function createGrid () {
+	clear (0, 0, WIDTH, HEIGHT);
+	ctx.fillStyle = "white";
+	rect (0, 0, WIDTH, HEIGHT);
+	for (var i = -40000; i < 400; i += 40) {	
+		for (var j = 0; j < WIDTH; j += 40) {
+			var tmp = new Object();
+			tmp = {
+             x : j,
+						 y : i,
+						 w : 40,
+						 h : 40,
+						 directionY: 1
+			}
+			rects.push(tmp);
+			//rect (i, j, 40, 40);
+		}
+			//github.com/daniloaleixo/jogo_digital.gitonsole.log("here");
+		draw ();
+	}
+}
+
+function draw () {
+	clear (0, 0, WIDTH, HEIGHT);
+	for (var i = 0; i < rects.length; i++) {
+		var tmp = rects[i];
+		ctx.strokeRect (tmp.x, tmp.y, tmp.w, tmp.w);
+	}
+}
+requestAnimationFrame (animate);
+
+function animate (time) {
+	for (var i = 0; i < rects.length; i++) {
+		rects[i].y += rects[i].directionY;
+	}
+
+	draw (rects);
+
+	requestAnimationFrame (animate);
+}
+
+function relMouseCoords (event) {
+	var totalOffsetX = 0;
+	var totalOffsetY = 0;
+	var canvasX = 0;
+	var canvasY = 0;
+	var currentElement = this;
+
+	do {
+		totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+		totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+	}
+	while (currentElement = currentElement.offsetParent)
+
+		canvasX = event.pageX - totalOffsetX;
+		canvasY = event.pageY - totalOffsetY;
+
+	if (event.offsetX !== undefined && event.offsetY !== undefined) {
+		//	return {x:event.offsetX, y:event.offsetY};
+		console.log ('x: ', event.offsetX, ' y: ', event.offsetY);
+		canvasX = event.offsetX; 
+		canvasY = event.offsetY;
+	}
+	changeColor (canvasX-canvasX%40, canvasY-canvasY%40);
+}
+
+function changeColor (xx, yy) {
+	ctx.fillStyle = "yellow";
+	console.log ('posicaoes :', xx, yy);
+	rect (xx, yy, 40, 40);
+	console.log ('click');
+}
+
+init();
+window.addEventListener ('click', relMouseCoords, true);
