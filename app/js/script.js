49cc74c7628c20d7d4922535ea18a973d1d9c12b
Danilo Aleixo Gomes de Souza <daniloaleixo@php.linux.ime.usp.br>
2016-12-01 18:34:27 -0200

    front page pronta, falta pegar os cliques

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,7 @@ var HEIGHT = 400;
 var rects = new Array();
 var numSquaresHor = 8, numSquaresVer = 8;
 var sizeSquare = 50;
+var fontSizeTitle = WIDTH/12;
 
 
 var mainGame = null;
@@ -16,11 +17,15 @@ var timerId = 0;
 timerId = setInterval(update, 1000/frames);
 
 var bgImage = new Image();
-var playImage = new Image();
-var instructImage = new Image();
-bgImage.src = "assets/visual/sprites/@4x/sprite-w.png"
-playImage.src = "assets/visual/sprites/@2x/sprite-tr.png"
-instructImage.src = "assets/visual/sprites/@2x/sprite-tl.png"
+var spriteTL = new Image();
+var spriteB = new Image();
+var spriteTR = new Image();
+var spriteBR = new Image();
+bgImage.src = "assets/visual/sprites/@2x/sprite-w.png"
+spriteTL.src = "assets/visual/sprites/@2x/sprite-tl.png"
+spriteB.src = "assets/visual/sprites/@2x/sprite-b.png"
+spriteTR.src = "assets/visual/sprites/@2x/sprite-tr.png"
+spriteBR.src = "assets/visual/sprites/@2x/sprite-br.png"
 
 var buttonX = [40,140,249];
 var buttonY = [240,240,240];
@@ -30,11 +35,14 @@ var buttonHeight = [40,40,40];
 bgImage.onload = function(){
     context.drawImage(bgImage, 0, 0);
 };
-playImage.onload = function(){
-    context.drawImage(playImage, buttonX[0], buttonY[0]);
+spriteTL.onload = function(){
+    context.drawImage(spriteTL, buttonX[0], buttonY[0]);
 };
-instructImage.onload = function(){
-    context.drawImage(instructImage, buttonX[1], buttonY[1]);
+spriteB.onload = function(){
+    context.drawImage(spriteB, buttonX[1], buttonY[1]);
+};
+spriteTR.onload = function(){
+    context.drawImage(spriteTR, buttonX[1], buttonY[1]);
 };
 
 function update() {
@@ -61,14 +69,66 @@ function draw(){
 	//context.drawImage(bgImage, 0, backgroundY);
 	//context.drawImage(bgImage, 0, 0);
 	drawBG();
-	context.drawImage(playImage, buttonX[0], buttonY[0]);
-	context.drawImage(instructImage, buttonX[1], buttonY[1]);
+	drawTitle();
+	drawLogo();
+	drawTapContinue();
+	drawBottomMenu();
 }
 
 function drawBG(){
 	clear (0, 0, WIDTH, HEIGHT);
 	for (var i = 0; i < numSquaresHor * sizeSquare ; i+=sizeSquare) {
 		for(var j = 0; j < numSquaresVer * sizeSquare; j+= sizeSquare)
-			context.drawImage (bgImage, i, j, 50, 50);
+			context.drawImage (bgImage, i, j, sizeSquare, sizeSquare);
 	}
+}
+
+function drawTitle(){
+	context.font="bold " + fontSizeTitle + "px Georgia";
+	context.textAlign = "center";
+	context.fillStyle = "#607d8b"
+	context.fillText("Nome",WIDTH/2,HEIGHT/5);
+	context.fillText("do Jogo",WIDTH/2,HEIGHT/5+fontSizeTitle);
+}
+
+function drawLogo(){
+	context.drawImage(spriteTL, WIDTH/numSquaresHor * 2 + sizeSquare/2, 
+								HEIGHT/numSquaresVer * 3, sizeSquare, sizeSquare);
+	context.drawImage(spriteB, WIDTH/numSquaresHor * 3 + sizeSquare/2, 	
+								HEIGHT/numSquaresVer * 3, sizeSquare, sizeSquare);
+	context.drawImage(spriteTR, WIDTH/numSquaresHor * 3 + sizeSquare/2, 	
+								HEIGHT/numSquaresVer * 4, sizeSquare, sizeSquare);
+
+	context.drawImage(spriteTR, WIDTH/numSquaresHor * 4 + sizeSquare/2, 
+								HEIGHT/numSquaresVer * 3, sizeSquare, sizeSquare);
+	context.drawImage(spriteB, WIDTH/numSquaresHor * 4 + sizeSquare/2, 
+								HEIGHT/numSquaresVer * 4, sizeSquare, sizeSquare);
+	context.drawImage(spriteBR, WIDTH/numSquaresHor * 5 + sizeSquare/2, 
+								HEIGHT/numSquaresVer * 4, sizeSquare, sizeSquare);
+
+	context.strokeStyle = "#ff5252";
+	context.lineWidth=sizeSquare/5;
+	context.globalAlpha = 0.8;
+	context.strokeRect(WIDTH/numSquaresHor * 3 + sizeSquare/2 - sizeSquare/10,
+		HEIGHT/numSquaresVer * 4 - sizeSquare/10,
+		sizeSquare + sizeSquare/5,
+		sizeSquare+sizeSquare/5);
+	context.globalAlpha = 1.0;
+}
+
+function drawTapContinue(){
+	context.font="bold " + fontSizeTitle/2 + "px Georgia";
+	context.textAlign = "center";
+	context.fillStyle = "#607d8b"
+	context.fillText("Toque para continuar",WIDTH/2,HEIGHT/10 * 7);
+}
+
+
+function drawBottomMenu(){
+	context.font="bold " + fontSizeTitle/1.8 + "px Georgia";
+	context.textAlign = "center";
+	context.fillStyle = "#607d8b"
+	context.fillText("Ajustes",WIDTH/2 - fontSizeTitle/1.8 * 5 ,HEIGHT/7 * 6);
+	context.fillText("Sobre",WIDTH/2,HEIGHT/7 * 6);
+	context.fillText("Créditos",WIDTH/2 + fontSizeTitle/1.8 * 5,HEIGHT/7 * 6);
 }
